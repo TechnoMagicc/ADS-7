0d5efb927208a0202ca07b77beed2d69f9a782f4
TechnoMagicc <leyzarof@gmail.com>
2025-05-12 23:31:55 +0300

    Add files via upload

--- include/train.h
+++ include/train.h
@@ -1,20 +1,25 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 NNTU-CS AND V47
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+
+#include <iostream>
+#include <random>
+
 class Train {
  private:
   struct Car {
     bool light;
     Car *next;
     Car *prev;
-    explicit Car(bool check) : light(check), next(nullptr), prev(nullptr) {}
   };
   int countOp;
   Car *first; 
  public:
   Train();
+  ~Train();
   void addCar(bool light);
   int getLength();        
   int getOpCount();       
+  void resetOpCount();
 };
 #endif  // INCLUDE_TRAIN_H_
