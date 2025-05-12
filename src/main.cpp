3062f314e46f462aa1d8952dd4ed82cb1a96979c
TechnoMagicc <leyzarof@gmail.com>
2025-05-12 23:13:41 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,15 @@
+// Copyright 2022 NNTU-CS
 #include <iostream>
-#include <chrono>
-#include <vector>
-#include <random>
 #include "train.h"
-#include "matplotlibcpp.h"
-
-namespace plt = matplotlibcpp;
-
-void performExperiment(int n, std::vector<int>& opCounts, std::vector<double>& times, bool allLightsOn, bool allLightsOff) {
-    Train train;
-    for (int i = 0; i < n; i++) {
-        bool light = allLightsOn ? true : (allLightsOff ? false : rand() % 2);
-        train.addCar(light);
-    }
-    auto start = std::chrono::high_resolution_clock::now();
-    int ops = train.getLength();
-    auto end = std::chrono::high_resolution_clock::now();
-    
-    opCounts.push_back(train.getOpCount());
-    times.push_back(std::chrono::duration<double, std::milli>(end - start).count());
-}
 
 int main() {
-    std::vector<int> opCounts;
-    std::vector<double> times;
-    std::vector<int> lengths;
-    for (int n = 2; n <= 100; n += 10) {
-        performExperiment(n, opCounts, times, true, false); 
-        performExperiment(n, opCounts, times, false, true); 
-        performExperiment(n, opCounts, times, false, false);
-        lengths.push_back(n);
-    }
-    plt::figure();
-    plt::title("Количество операций от длины поезда");
-    plt::xlabel("Длина поезда");
-    plt::ylabel("Количество операций");
-    plt::plot(lengths, opCounts, "r-");
-    plt::save("result/plot-1.png");
+  Train train;
+  int count = 60; // кол-во вагонов
 
-    plt::figure();
-    plt::title("Время вычисления от длины поезда");
-    plt::xlabel("Длина поезда");
-    plt::ylabel("Время (мс)");
-    plt::plot(lengths, times, "b-");
-    plt::save("result/plot-2.png");
+  while (count--)
+    train.addCar(false);
 
-    return 0;
+  std::cout << train.getLength() << std::endl;
+  std::cout << train.getOpCount() << std::endl;
+  return 0;
 }
